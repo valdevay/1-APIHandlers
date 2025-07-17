aeebe32f42ddaa8996a47e3c99abd17b561fa099
Vladimir <valdevay@gmail.com>
2025-07-17 22:02:06 +0300

    Clean up main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"log"
+
+	"example.com/mymodule/internal/db"
+	"example.com/mymodule/internal/handlers"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	initDB()
+	database, err := db.InitDB()
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
 	e := echo.New()
+
+	taskRepo := taskService.NewTaskRepository(database)
+	taskService := taskService.NewTaskService(taskRepo)
+	taskHandler := handlers.NewTaskHandler(taskService)
 
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 
-	e.GET("/get", getTask)
-	e.POST("/post", createTask)
-	e.PUT("/put/:id", updateTask)
-	e.DELETE("/delete/:id", deleteTask)
+	e.GET("/get", taskHandler.GetTasks)
+	e.POST("/post", taskHandler.CreateTask)
+	e.PUT("/put/:id", taskHandler.UpdateTask)
+	e.DELETE("/delete/:id", taskHandler.DeleteTask)
 
 	e.Start("localhost:8080")
 }
